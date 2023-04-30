05a8c5135811ff6cad98266980a8b87e928c264b
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-04-30 13:02:57 -0400

    add views for index

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 require('dotenv').config()
-const breadController = require('/controllers/breads')
+const breadController = require('./controllers/bread')
 
 const app   = express()
+
+//middleware
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // routes
 app.use('/breads', breadController)
