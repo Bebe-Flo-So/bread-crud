624cd1de097284cfd18106e4c3d4a03e9ed5432e
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-05-06 23:38:01 -0400

    added edit

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
 
-
+//GET all the bread
 router.get('/', (req, res) => {
     res.render('index', {
         breads: Bread
@@ -12,15 +12,22 @@ router.get('/new', (req, res) => {
     res.render('new')
 })
 
-//GET bread a specific bread by index
+// GET bread a specific bread by index
 router.get('/:index', (req, res) => {
     const { index } = req.params
     res.render('show', {
         bread: Bread[index],
-        index: index
+        index
     })
 })
 
+router.get('/:index/edit', (req, res) => {
+    const { index } = req.params
+    res.render('edit', {
+        bread: Bread[index],
+        index
+    })
+})
 
 router.post('/', (req, res) => {
     if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
@@ -32,7 +39,7 @@ router.post('/', (req, res) => {
     }
 
     Bread.push(req.body)
-    res.redirect('/breads')
+    res.status(303).redirect('/breads')
 })
 
 router.delete('/:index', (req, res) => {
@@ -40,6 +47,20 @@ router.delete('/:index', (req, res) => {
     Bread.splice(index, 1)
     res.status(303).redirect('/breads')
   }) 
+
+  router.put('/:index', (req, res) => {
+    const { index } = req.params
+    if (!req.body.image) req.body.image = 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg'
+
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[index] = req.body
+    res.status(303).redirect(`/breads/${index}`)
+
+  })
 
 
 module.exports = router
