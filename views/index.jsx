d4c732d58c8a229c04b933c94f5f2c09bbaacb05
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-05-20 23:47:38 -0400

    added baker bio and other stuff

--- views/index.jsx
+++ views/index.jsx
@@ -1,15 +1,21 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Index({ breads }) {
-    const display = breads.map((bread) => {
+function Index({ breads, bakers }) {
+    const breadDisplay = breads.map((bread) => {
         return (
             <li key={bread.id}>
-        <a href={`/breads/${bread.id}`}>{bread.name}</a>
+                <a href={`/breads/${bread.id}`}>{bread.name}</a>
         </li>
         )
     })
-
+    const bakerDisplay = bakers.map(baker => {
+      return (
+        <li key={baker.id}>
+            <a href={`/baker/${baker.id}`}>{baker.name}</a>
+        </li>
+      )
+    })
     return (
       <Default>
         <h2>Index Page</h2>
@@ -20,8 +26,13 @@ function Index({ breads }) {
             </button>
           </a>
         </div>
+        <h3>Bakers</h3>
         <ul>
-            {display}
+           {bakerDisplay}
+        </ul>
+        <h3>Bread</h3>
+        <ul>
+          {breadDisplay}
         </ul>
       </Default>
     )
